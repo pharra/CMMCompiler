b2e9a0b73c1932760eb0b728d8824f91d605095e
pharra <typechasing@gmail.com>
2018-10-09 19:57:13 +0800

    print more info about token.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 
-#include "CMMLexer/Regex.h"
+#include "CMMLexer/Lexer.h"
+#include "Structure/Token.h"
 
 int main(int argc, char *argv[]) {
-    if(argc != 2) {
+    if (argc != 2) {
         std::cout << "need two params" << std::endl;
+        return 1;
+    }
+    Lexer lexer = Lexer(argv[1]);
+    Token token = lexer.getNext();
+    while (token.getTag() != END) {
+        std::cout << "<Line:" << token.getLine() << ", Column:" << token.getColumn() << ", " <<token.getTagName() << ", " <<token.getValue() << ">\n";
+        token = lexer.getNext();
     }
     return 0;
 }
