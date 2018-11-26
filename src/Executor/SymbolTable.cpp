2088278db05f768605f94e94a701f84959c3fcc0
pharra <typechasing@gmail.com>
2018-11-26 16:23:09 +0800

    add symbolTable class

---
+++ src/Executor/SymbolTable.cpp
@@ -0,0 +1,5 @@
+//
+// Created by chasi on 2018/11/26.
+//
+
+#include "SymbolTable.h"
---
+++ src/Executor/SymbolTable.h
@@ -0,0 +1,14 @@
+//
+// Created by chasi on 2018/11/26.
+//
+
+#ifndef CMMCOMPILER_SYMBOLTABLE_H
+#define CMMCOMPILER_SYMBOLTABLE_H
+
+
+class SymbolTable {
+
+};
+
+
+#endif //CMMCOMPILER_SYMBOLTABLE_H
