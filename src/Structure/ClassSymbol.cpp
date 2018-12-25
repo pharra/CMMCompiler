fed9904efe639392b4dde477b26e5d44259a19c2
pharra <typechasing@gmail.com>
2018-12-25 13:29:16 +0800

    add ClassSymbol

--- src/Structure/ClassSymbol.cpp
+++ src/Structure/ClassSymbol.cpp
@@ -5,5 +5,32 @@
 #include "ClassSymbol.h"
 
 ClassSymbol::~ClassSymbol() {
+};
 
+ClassSymbol::ClassSymbol(const std::string &name, SymbolType type, _dataSize line, int level) : AbstractSymbol(name,
+                                                                                                               type,
+                                                                                                               line,
+                                                                                                               level) {
+
+}
+
+Object *ClassSymbol::getObject() const {
+    return object;
+}
+
+void ClassSymbol::setObject(Object *object) {
+    ClassSymbol::object = object;
+}
+
+bool ClassSymbol::addAttr(std::string name, Value &value) {
+    auto result = attrs.insert(std::map<std::string, Value>::value_type(name, value));
+    return result.second;
+}
+
+const Value *ClassSymbol::getAttr(std::string name) const {
+    auto i = attrs.find(name);
+    if (i != attrs.end()) {
+        return &(i->second);
+    }
+    return nullptr;
 }
--- src/Structure/ClassSymbol.h
+++ src/Structure/ClassSymbol.h
@@ -5,10 +5,29 @@
 #ifndef CMMCOMPILER_CLASSSYMBOL_H
 #define CMMCOMPILER_CLASSSYMBOL_H
 
+#include <map>
+
 #include "AbstractSymbol.h"
+#include "Object.h"
+#include "Value.h"
 
 class ClassSymbol : public AbstractSymbol {
+public:
     ~ClassSymbol() override;
+
+    ClassSymbol(const std::string &name, SymbolType type, _dataSize line, int level);
+
+    Object *getObject() const;
+
+    void setObject(Object *object);
+
+    bool addAttr(std::string name, Value &value);
+
+    const Value *getAttr(std::string name) const;
+
+private:
+    std::map<std::string, Value> attrs;
+    Object *object;
 };
 
 
